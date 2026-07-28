2dd1c2f8fda3faef72809eeb31283ec0747648df
João Marcus Braga dos Santos <joaomarcusbraga@hotmail.com>
2026-07-28 20:00:14 -0300

    SEO and adjust on prisma config

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -2,12 +2,20 @@ import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
+const databaseUrl = process.env.DATABASE_URL as string;
+
+if (!databaseUrl) {
+  throw new Error("A variável DATABASE_URL não foi encontrada no .env!");
+}
+
+const dbUrl = new URL(databaseUrl);
+
 const adapter = new PrismaMariaDb({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "65537",
-  database: "colisaocobblemon",
+  host: dbUrl.hostname,
+  port: Number(dbUrl.port) || 3306,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.substring(1),
 });
 
 const prisma = new PrismaClient({ adapter });
